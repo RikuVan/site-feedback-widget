ef874b68edb8925228f94e4497a62e65c6e26c5f
Richard Van Camp <richard.vancamp@gofore.com>
2019-05-12 17:54:07 +0300

    feat: allow passing of options to widget

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
+
 import App from './App.svelte'
 
-const widget = new App({
-  target: document.getElementById('site-feedback-widget'),
-  props: {
-    feedbackOffset: 700
-  }
-})
-
-export default widget
+window.site_feedback_widget = function(props) {
+  const id = props && props.id ? props.id : 'site-feedback-widget'
+  return new App({
+    target: document.getElementById(id),
+    props: {
+      ...props,
+      feedbackOffset: 700,
+      autoOpen: true
+    }
+  })
+}
